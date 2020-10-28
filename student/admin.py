dbf31df9e5cad0f2f41b94d6ecfb833d50d734ba
mosaadcode <[codes.codeone@gmail.com]>
2020-10-28 11:58:43 +0200

    change student view

--- student/admin.py
+++ student/admin.py
@@ -20,8 +20,8 @@ class StudentAdmin(ImportExportMixin, UserAdmin):
     fieldsets = (
         (None, { 'fields': ('code', 'username', ('school', 'grade'),'password', ('is_active', 'can_pay', 'bus_active'))}),
         # (None, { 'fields': (('is_staff','is_admin'),)}),
-        ('الأقساط والسداد', {'fields': (('study_payment1', 'study_payment3'), 'bus_payment2',('total_paid','payment_status'))}),
         ('التواصل', {'fields': ('message', ('father_mobile','mother_mobile'),('phone_number', 'email'),('living_area', 'address', 'old_bus'), 'last_login')}),
+        ('الأقساط والسداد', {'fields': (('study_payment1', 'study_payment3'), 'bus_payment2',('total_paid','payment_status'))}),
         # ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_admin', 'groups', 'user_permissions')}),
                  )
     resource_class = StudentResource
