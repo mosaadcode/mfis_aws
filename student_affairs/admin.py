2532860f8592418eda4dc94f463d028c98cd894d
mosaadcode <[codes.codeone@gmail.com]>
2021-08-01 21:54:25 +0200

    student affairs 3

--- student_affairs/admin.py
+++ student_affairs/admin.py
@@ -1,3 +1,105 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Student, School,Governorate, Nationality, Class, Class_group
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+class GovernorateAdmin(ImportExportModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    def has_module_permission(self, request):
+        if request.user.is_authenticated:
+            if request.user.code in ('mosaad'):
+                return True
+            return False
+    # def has_delete_permission(self, request, obj=None):
+    #     if request.user.code == "mosaad":
+    #         return True
+    #     return False
+
+class NationalityAdmin(ImportExportModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    def has_module_permission(self, request):
+        if request.user.is_authenticated:
+            if request.user.code in ('mosaad'):
+                return True
+            return False
+    # def has_delete_permission(self, request, obj=None):
+    #     if request.user.code == "mosaad":
+    #         return True
+    #     return False
+
+class GroupAdmin(ImportExportModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    def has_module_permission(self, request):
+        if request.user.is_authenticated:
+            if request.user.code in ('mosaad','affb','affg'):
+                return True
+            return False
+
+class ClassAdmin(ImportExportModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    def has_module_permission(self, request):
+        if request.user.is_authenticated:
+            if request.user.code in ('mosaad','affb','affg'):
+                return True
+            return False
+
+class StudentAdmin(ImportExportModelAdmin):
+    list_display = ('code', 'name','grade','status','age1oct','father_mobile','mother_mobile','phone_number',)
+    autocomplete_fields = ['birth_gov','nationality']
+    search_fields = ('code','name','father_id',)
+    readonly_fields = ('code','age1oct','start_year',)
+
+    filter_horizontal = ()
+    list_filter = ('school','grade','status','is_over',)
+
+    fieldsets = (
+        ('بيانات الطالب', { 'fields': ('name','en_name','student_id',('birth_date', 'age1oct'),'birth_gov',('nationality','religion'))}),
+        ('بيانات الالتحاق', { 'fields': ('code','school','start_year','study_year', ('status','from_to'),'status_no', 'grade',('Class','group','is_over'),'global_code')}),
+        ('بيانات ولي الامر', { 'fields': ('responsibility','father_name','father_job','father_id','mother_name','father_mobile','mother_mobile','phone_number','address_1' ,'email','notes')}),
+
+                 )
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            if obj.code != "":
+                return self.readonly_fields + ('study_year','school')
+            return self.readonly_fields
+        return self.readonly_fields
+
+    # def has_delete_permission(self, request, obj=None):
+    #     if request.user.code in ('mosaad','mosaad2'):
+    #         return True
+    #     return False
+    # def has_change_permission(self, request, obj=None):
+    #     if request.user.code in ('mosaad','mosaad2'):
+    #         return True
+    #     return False
+
+    def has_module_permission(self, request):
+        if request.user.is_authenticated:
+            if request.user.code in ('mosaad','affb','affg'):
+                return True
+            return False
+    def has_delete_permission(self, request, obj=None):
+        if request.user.code == "mosaad":
+            return True
+        return False
+
+class SchoolAdmin(ImportExportModelAdmin):
+     
+    def has_module_permission(self, request):
+        if request.user.is_authenticated:
+            if request.user.code in ('mosaad',):
+                return True
+            return False
+
+admin.site.register(Student,StudentAdmin)
+admin.site.register(School,SchoolAdmin)
+admin.site.register(Class,ClassAdmin)
+admin.site.register(Class_group,GroupAdmin)
+admin.site.register(Governorate,GovernorateAdmin)
+admin.site.register(Nationality,NationalityAdmin)
