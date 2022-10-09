235f239bd3563c8b2a444b73a0afc330677a05bc
mosaadcode <[codes.onecode@gmail.com]>
2022-10-09 20:36:54 +0200

    add contact admin

--- student_affairs/admin.py
+++ student_affairs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin
-from .models import Student, School,Governorate, Nationality, Class, Class_group
+from .models import Student, School,Governorate, Nationality, Class, Class_group, Contact
 from import_export.admin import ImportExportModelAdmin
 from student.resources import StudentAffResource
 from django.utils.translation import ngettext
@@ -66,7 +66,7 @@ class StudentAdmin(ImportExportModelAdmin):
     fieldsets = (
         ('بيانات الطالب', { 'fields': ('name','en_name',('student_id','kind'),('birth_date', 'age1oct'),'birth_gov',('nationality','religion'))}),
         ('بيانات الالتحاق', { 'fields': (('study_year','payment_status'),('start_year','start_grade'),('school','code'), 'grade', ('status','from_to'),'status_no',('Class','group','is_over'),('global_code','document_status'))}),
-        ('بيانات ولي الامر', { 'fields': ('responsibility',('father_name','father_job'),('father_id','father_mobile'),('mother_name','mother_job'),'mother_mobile',('phone_number','phone_number2'),('address_1' ,'email'),'notes')}),
+        ('بيانات ولي الامر', { 'fields': (('responsibility','contact_status'),('father_name','father_job'),('father_id','father_mobile'),('mother_name','mother_job'),'mother_mobile',('phone_number','phone_number2'),('address_1' ,'email'),'notes')}),
 
                  )
     resource_class = StudentAffResource
@@ -218,6 +218,10 @@ class StudentAdmin(ImportExportModelAdmin):
                                 self.log_change(request, obj, 'تم التحويل من المدرسة')
                                 transferd +=1
                                 obj.save()
+
+                                StudentAcc.objects.filter(code=obj.code).update(
+                                bus_active=False,)
+
                                 try:
                                     StudentFees = Fee.objects.filter(student__code=StudentCode)
                                     StudentFees.update(school = NewSchool)
@@ -277,9 +281,110 @@ class SchoolAdmin(ImportExportModelAdmin):
                 return True
             return False
 
+class ContactAdmin(ImportExportModelAdmin):
+    list_display = ('student', 'father_mobile', 'mother_mobile', 'phone_number','phone_update','address_update')
+    autocomplete_fields = ['student']
+    search_fields = ('student__code','student__name')
+    readonly_fields = ('student', 'father_mobile', 'mother_mobile', 'phone_number','email','address_1','school','phone_update','address_update')
+    filter_horizontal = ()
+    list_filter = ('school','phone_update', 'address_update' )
+    fieldsets = ()
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.code == "affg":
+            return qs.filter(school__in = ('.بنات.', 'بنات'))
+        elif request.user.code =="affb":
+            return qs.filter(school__in = ('بنين',))
+        return qs
+
+    def has_module_permission(self, request):
+        if request.user.is_authenticated:
+            if request.user.code in ('mosaad','affb','affg'):
+                return True
+            return False
+    def has_delete_permission(self, request, obj=None):
+        if request.user.code == "mosaad":
+            return True
+        return False
+
+    def PhoneUpdate(self, request, queryset):
+        update = 0
+        passed = 0
+        for obj in queryset:
+            if obj.phone_update == False:
+                passed +=1
+            else:
+                try:
+                    student = Student.objects.get(id=obj.student_id)
+                    student.father_mobile=obj.father_mobile
+                    student.mother_mobile = obj.mother_mobile
+                    student.phone_number = obj.phone_number
+                    student.contact_status = True
+                    obj.phone_update = False
+                    self.log_change(request, student, 'تم تحدبث ارقام الهاتف')
+                    student.save()
+                    obj.save()
+                    if obj.phone_update == False and obj.address_update == False :
+                        obj.delete()
+                    update +=1
+                except Student.DoesNotExist:
+                    pass
+                if update != 0:
+                    self.message_user(request, ngettext(
+                    'تم تحديث بيانات %d طالب ',
+                    'تم تحديث بيانات %d طالب ',
+                    update,
+                    ) % update, messages.SUCCESS)
+                if passed != 0:
+                    self.message_user(request, ngettext(
+                    'لا يمكن  تحديث بيانات %d طالب',
+                    'لا يمكن  تحديث بيانات %d طالب',
+                    passed,
+                    ) % passed, messages.ERROR)
+
+    def AddressUpdate(self, request, queryset):
+        update = 0
+        passed = 0
+        for obj in queryset:
+            if obj.address_update == False:
+                passed +=1
+            else:
+                try:
+                    student = Student.objects.get(id=obj.student_id)
+                    student.email=obj.email
+                    student.address_1 = obj.address_1
+                    student.contact_status = True
+                    obj.address_update = False
+                    self.log_change(request, student, 'تم تحدبث بيانات العنوان')
+                    student.save()
+                    obj.save()
+                    if obj.phone_update == False and obj.address_update == False :
+                        obj.delete()
+                    update +=1
+                except Student.DoesNotExist:
+                    pass
+                if update != 0:
+                    self.message_user(request, ngettext(
+                    'تم تحديث بيانات %d طالب ',
+                    'تم تحديث بيانات %d طالب ',
+                    update,
+                    ) % update, messages.SUCCESS)
+                if passed != 0:
+                    self.message_user(request, ngettext(
+                    'لا يمكن  تحديث بيانات %d طالب',
+                    'لا يمكن  تحديث بيانات %d طالب',
+                    passed,
+                    ) % passed, messages.ERROR)
+                    
+    AddressUpdate.short_description = 'تحديث بيانات العنوان'
+    PhoneUpdate.short_description = 'تحديث ارقام الهاتف'
+    actions = ['PhoneUpdate','AddressUpdate']
+
 admin.site.register(Student,StudentAdmin)
 admin.site.register(School,SchoolAdmin)
 admin.site.register(Class,ClassAdmin)
 admin.site.register(Class_group,GroupAdmin)
 admin.site.register(Governorate,GovernorateAdmin)
 admin.site.register(Nationality,NationalityAdmin)
+admin.site.register(Contact,ContactAdmin)
