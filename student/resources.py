72c499be05ad083689df246baa5c3630128d8981
mosaadcode <[codes.onecode@gmail.com]>
2022-10-30 11:20:54 +0200

    studentAff resource

--- student/resources.py
+++ student/resources.py
@@ -129,9 +129,9 @@ class StudentAffResource(resources.ModelResource):
         model = StudentAff
         import_id_fields = ('code',)
         fields = ('code','study_year','school','grade','Class','status','status_no','name','en_name','student_id','birth_date','age1oct','kind','nationality','address_1','phone_number','phone_number2',
-        'mother_mobile','father_mobile','email','father_id','father_name','father_job','mother_name','mother_job','payment_status','global_code','document_status','contact_status')
+        'mother_mobile','father_mobile','email','father_id','father_name','father_job','mother_name','mother_job','payment_status','global_code','document_status','contact_status','is_over')
         export_order = ('code','study_year','school','grade','Class','status','status_no','name','en_name','student_id','birth_date','age1oct','kind','nationality','address_1','phone_number','phone_number2',
-        'mother_mobile','father_mobile','email','father_id','father_name','father_job','mother_name','mother_job','payment_status','global_code','document_status','contact_status')
+        'mother_mobile','father_mobile','email','father_id','father_name','father_job','mother_name','mother_job','payment_status','global_code','document_status','contact_status','is_over')
 
 class BusStudentResource(resources.ModelResource):
 
