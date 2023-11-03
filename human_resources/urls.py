703ce57e042405150046a3a7e7a8799190ef9a2b
mosaadcode <codes.onecode@gmail.com>
2023-11-03 22:41:33 +0200

    fix hr urls for vacation

--- human_resources/urls.py
+++ human_resources/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+# from . import admin
 
 urlpatterns = [
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('vacation/', views.vacation, name='vacation'),
     path('contact/', views.employee_contact, name='employee_contact'),
     path('delete_permission/<int:permission_id>/', views.delete_permission, name='delete_permission'),
+    path('delete_vacation/<int:vacation_id>/', views.delete_vacation, name='delete_vacation'),
 
 ]
